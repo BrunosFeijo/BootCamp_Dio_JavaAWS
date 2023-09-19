5954e605c72d3d34bac08d216c6dd2f56c330b26
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2023-09-18 22:28:24 -0300

    JUnit

--- Testes_Unitarios_JUnit/src/test/java/org/example/PessoaTest.java
+++ Testes_Unitarios_JUnit/src/test/java/org/example/PessoaTest.java
@@ -7,22 +7,21 @@ import java.time.LocalDateTime;
 
 public class PessoaTest {
     @Test
-    void deveCalcularIdadeCorretamente(){
-        Pessoa pessoa = new Pessoa("Thais", LocalDateTime.of(2000,1,1,15,0));
-        Assertions.assertEquals(23,pessoa.getIdade());
+    void deveCalcularIdadeCorretamente() {
+        Pessoa pessoa = new Pessoa("Thais", LocalDateTime.of(2000, 1, 1, 15, 0));
+        Assertions.assertEquals(23, pessoa.getIdade());
     }
     @Test
-    void deveRetornarSeEhMaiorDeIdade(){
-        Pessoa pessoa = new Pessoa("Thais", LocalDateTime.of(2000,1,1,15,0));
+    void deveRetornarSeEhMaiorDeIdade() {
+        Pessoa pessoa = new Pessoa("Thais", LocalDateTime.of(2000, 1, 1, 15, 0));
         Assertions.assertTrue(pessoa.ehMaiorDeIdade());
 
-        Pessoa pessoa1 = new Pessoa("João",LocalDateTime.now());
+        Pessoa pessoa1 = new Pessoa("João", LocalDateTime.now());
         Assertions.assertFalse(pessoa1.ehMaiorDeIdade());
     }
     @Test
-    void validaIgualdade(){
-        Pessoa pessoa =new Pessoa("Pessoa 1",LocalDateTime.now());
-        Assertions.assertSame(pessoa,pessoa);
+    void validaIgualdade() {
+        Pessoa pessoa = new Pessoa("Pessoa 1", LocalDateTime.now());
+        Assertions.assertSame(pessoa, pessoa);
     }
-
 }
