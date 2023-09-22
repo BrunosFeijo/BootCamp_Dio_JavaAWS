2d40a42575241674fda3a62f552bb9c508b5d0be
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2023-09-21 22:41:38 -0300

    SpringBoot

---
+++ quick-start-maven/src/main/java/one/digitalinnovation/App.java
@@ -0,0 +1,15 @@
+package one.digitalinnovation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args) {
+        Parse testeComponente = new Parse();
+        testeComponente.message();
+
+        System.out.println("Teste");
+    }
+}
