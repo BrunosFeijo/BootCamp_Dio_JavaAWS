3314b95da4b9180d868c6c8f9b7a0caa130f9763
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2023-09-23 23:28:56 -0300

    Padroes

---
+++ lab-padroes-projeto-spring/src/main/java/one/digitalinnovation/gof/LabPadroesProjetoSpringApplication.java
@@ -0,0 +1,19 @@
+package one.digitalinnovation.gof;
+//Projeto Spring Boot gerado via Spring Initializr
+// Spring Data JPA
+// Spring Web
+// H2 DB
+// OpenFeign
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class LabPadroesProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabPadroesProjetoSpringApplication.class, args);
+	}
+
+}
